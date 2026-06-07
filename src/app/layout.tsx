bd919f02715b46e4dfdf6a9ca9a25c62927524f3
sujitshahi <shahisujit9@gmail.com>
2026-06-07 12:15:34 +0545

    deleted paritcle background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar/page";
-import ParticleBackground from "./ParticleBackground/page";
+
 
 
 
@@ -32,7 +32,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <ParticleBackground />
+  
         <Navbar />
         {children}
         </body>
