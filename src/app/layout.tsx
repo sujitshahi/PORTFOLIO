864fb0297a30df16d82c3f62c2ffe9565ef2f1d4
sujitshahi <shahisujit9@gmail.com>
2026-07-03 10:50:23 +0545

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       suppressHydrationWarning={true}
       >
   
-        <Navbar />
+        <Navbar />   
         {children} 
         </body>
     </html>
