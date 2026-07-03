24a7d847b27f48c80d6333d88b2c4f2c8fd5cd8a
sujitshahi <shahisujit9@gmail.com>
2026-07-03 10:49:05 +0545

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar/page";
 
-
-
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
