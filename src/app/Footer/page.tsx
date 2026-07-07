f98d7aacf3bf66b51d364c334485c2f5a62a8b03
sujitshahi <shahisujit9@gmail.com>
2026-07-07 11:17:17 +0545

    changes in footer

--- src/app/Footer/page.tsx
+++ src/app/Footer/page.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <p>
-        © 2026 Sujit Shahi — Front-End Developer with Full Stack 
+        © 2026 Sujit Shahi — Front-End Developer with Full Stack Foundation
       </p>
     </footer>
   );
