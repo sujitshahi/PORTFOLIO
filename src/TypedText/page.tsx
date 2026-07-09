3ac89e26ed718b43b1c5f6a7a40655ab3587ad46
sujitshahi <shahisujit9@gmail.com>
2026-07-09 17:33:32 +0545

    changes

--- src/TypedText/page.tsx
+++ src/TypedText/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 
-const roles = ['Front-End Developer With Full-Stack Foundation.', 'UI/UX Enthusiast.', 'Problem Solver.'];
+const roles = ['Front-End Developer With Full-Stack Foundation.', 'UI/UX Enthusiast.', 'Problem Solver.', 'React Developer'];
 
 export default function TypedText() {
   const [roleIndex, setRoleIndex] = useState(0);
